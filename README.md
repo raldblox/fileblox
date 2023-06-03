34eac176d13d7fa041001bf83f5f66b29d60f48e
0xDanki <typepuller@gmail.com>
2023-06-04 03:34:55 +0800

    Initial contracts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# fileblox
-FileBlox
+# Fileblox
+FileBlox is a Decentralized marketplace for copyrighted content. It utilizes file encryption to enable creators to gate the content until a user buys it as an NFT. 
+
+The platform enables artists, photographers, and creators to sell their copyrighted files while ensuring protection against unauthorized use and promoting a fair ecosystem for the creator economy.
---
+++ scripts/deploy.js
@@ -0,0 +1,41 @@
+const hre = require('hardhat')
+const dotenv = require('dotenv')
+const fs = require('fs')
+
+function replaceEnvContractAddresses (marketplaceAddress, nftAddress, erc20Address, networkName) {
+  const envFileName = '.env.local'
+  const envFile = fs.readFileSync(envFileName, 'utf-8')
+  const env = dotenv.parse(envFile)
+  env[`MARKETPLACE_CONTRACT_ADDRESS_${networkName}`] = marketplaceAddress
+  env[`NFT_CONTRACT_ADDRESS_${networkName}`] = nftAddress
+  env[`ERC20_CONTRACT_ADDRESS_${networkName}`] = erc20Address
+  const newEnv = Object.entries(env).reduce((env, [key, value]) => {
+    return `${env}${key}=${value}\n`
+  }, '')
+
+  fs.writeFileSync(envFileName, newEnv)
+}
+
+async function main () {
+  process.env.IS_RUNNING = true
+  const Marketplace = await hre.ethers.getContractFactory('Marketplace')
+  const marketplace = await Marketplace.deploy()
+  await marketplace.deployed()
+  console.log('Marketplace deployed to:', marketplace.address)
+
+  const NFT = await hre.ethers.getContractFactory('NFT')
+  const nft = await NFT.deploy(marketplace.address)
+  await nft.deployed()
+  console.log('Nft deployed to:', nft.address)
+
+  console.log('Mock ApeCoin deployed to:', erc20.address)
+
+  replaceEnvContractAddresses(marketplace.address, nft.address, erc20.address, hre.network.name.toUpperCase())
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
