cfe1d3e5aa29aa90595046a88ff49202f121975e
raldblox <raldblox@outlook.com>
2023-06-04 14:13:16 +0800

    Updated seo header

--- react-web-app/components/Header.js
+++ react-web-app/components/Header.js
@@ -4,21 +4,21 @@ import React from 'react'
 const Header = () => {
     return (
         <Head>
-            <title>FileBlox</title>
-            <meta name="description" content="" />
+            <title>FileBlox | A decentralized marketplace for copyrighted content</title>
+            <meta name="description" content="A decentralized marketplace for copyrighted content" />
             <meta name="author" content="Team FileBlox" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content="" />
-            <meta property="og:title" content="Advancing Society through the Power of Emerging Technologies." />
-            <meta property="og:description" content="" />
+            <meta property="og:title" content="FileBlox" />
+            <meta property="og:description" content="Fileblox: A decentralized marketplace for copyrighted content" />
             <meta property="og:image" content="" />
             <meta property="og:image:alt" content="FileBlox" />
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="@FileBlox" />
             <meta property="twitter:title" content="" />
-            <meta property="twitter:description" content="" />
+            <meta property="twitter:description" content="Fileblox: A decentralized marketplace for copyrighted content" />
             <meta property="twitter:image" content="" />
             <link rel="apple-touch-icon" content="" />
         </Head>
