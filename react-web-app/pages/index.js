98df69346d27815cf3811124a11677242989d0f4
raldblox <raldblox@outlook.com>
2023-06-05 06:46:09 +0800

    Added featured section

--- react-web-app/pages/index.js
+++ react-web-app/pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import HeroSection from '@/components/sections/HeroSection'
+import FeatureSection from '@/components/sections/FeatureSection'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <HeroSection />
+      <FeatureSection />
     </>
   )
 }
