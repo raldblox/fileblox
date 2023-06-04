279a4035f2c588d69f6ce45a130ee651eacad493
raldblox <raldblox@outlook.com>
2023-06-05 06:57:55 +0800

    Added stats section

--- react-web-app/pages/index.js
+++ react-web-app/pages/index.js
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import HeroSection from '@/components/sections/HeroSection'
 import FeatureSection from '@/components/sections/FeatureSection'
+import StatsSection from '@/components/sections/StatsSection'
+import Testimonials from '@/components/sections/Testimonials'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +12,8 @@ export default function Home() {
     <>
       <HeroSection />
       <FeatureSection />
+      <StatsSection />
+      <Testimonials />
     </>
   )
 }
