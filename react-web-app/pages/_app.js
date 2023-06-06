bac479131885d5af555f58119037f8ea68c84ace
raldblox <raldblox@outlook.com>
2023-06-06 11:00:16 +0800

    Added context provider

--- react-web-app/pages/_app.js
+++ react-web-app/pages/_app.js
@@ -1,10 +1,13 @@
 import Layout from "@/components/Layout";
+import { ContextProvider } from "@/context";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ContextProvider>
   );
 }
