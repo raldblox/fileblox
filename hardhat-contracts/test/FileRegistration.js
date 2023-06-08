2c2f24bc02cb9eb66d6cfb936a8d8c22e411ced2
raldblox <raldblox@outlook.com>
2023-06-08 15:43:07 +0800

    Added test script for mintToken and payForFile

--- hardhat-contracts/test/FileRegistration.js
+++ hardhat-contracts/test/FileRegistration.js
@@ -3,95 +3,116 @@ const { expect } = require("chai");
 
 describe("FileRegistry", function () {
     let fileRegistry;
-    let nft;
+    let token;
     let owner;
+    let buyer;
 
     beforeEach(async function () {
         const FileRegistry = await ethers.getContractFactory("FileRegistry");
-        [owner] = await ethers.getSigners();
+        [owner, buyer] = await ethers.getSigners();
         fileRegistry = await FileRegistry.deploy(owner.address);
         tokenAddess = await fileRegistry.getTokenAddress();
-        console.log(`Registry Address: ${fileRegistry.address}, Token Addess: ${tokenAddess}`)
+        token = await ethers.getContractAt("NFT", tokenAddess);
     });
 
-    it("Should record a file", async function () {
+    describe("File Registry System", function () {
         const filePath = "example/path";
         const fileName = "example_file.txt";
         const fileSize = 1024;
         const filePrice = 100;
         const fileType = "text/plain";
         const fileDescription = "Example file";
-        const isToken = false;
 
-        const tx = await fileRegistry.recordFile(
-            filePath,
-            fileName,
-            fileSize,
-            filePrice,
-            fileType,
-            fileDescription,
-            isToken
-        );
 
-        await tx.wait();
+        it("Should record a file", async function () {
+            const isToken = false;
+            const tx = await fileRegistry.recordFile(
+                filePath,
+                fileName,
+                fileSize,
+                filePrice,
+                fileType,
+                fileDescription,
+                isToken
+            );
 
-        const fileId = 0;
+            await tx.wait();
 
-        const [
-            retrievedFilePath,
-            retrievedFileSize,
-            retrievedFilePrice,
-            retrievedFileType,
-            retrievedFileName,
-            retrievedFileDescription,
-            retrievedUploader,
-        ] = await fileRegistry.getFileDataByFileID(fileId);
+            const fileId = 0;
 
-        expect(retrievedFilePath).to.equal(filePath);
-        expect(retrievedFilePrice).to.equal(filePrice);
+            const [
+                retrievedFilePath,
+                retrievedFileSize,
+                retrievedFilePrice,
+                retrievedFileType,
+                retrievedFileName,
+                retrievedFileDescription,
+                retrievedUploader,
+            ] = await fileRegistry.getFileDataByFileID(fileId);
+
+            expect(retrievedFilePath).to.equal(filePath);
+            expect(retrievedFilePrice).to.equal(filePrice);
+        });
+
+        it("Should record a file and mint token", async function () {
+            const isToken = true;
+            const tx = await fileRegistry.recordFile(
+                filePath,
+                fileName,
+                fileSize,
+                filePrice,
+                fileType,
+                fileDescription,
+                isToken
+            );
+            await tx.wait();
+            const fileId = 0;
+            const [
+                retrievedFilePath,
+                retrievedFileSize,
+                retrievedFilePrice,
+                retrievedFileType,
+                retrievedFileName,
+                retrievedFileDescription,
+                retrievedUploader,
+            ] = await fileRegistry.getFileDataByFileID(fileId);
+
+            expect(retrievedFilePath).to.equal(filePath);
+            expect(retrievedFilePrice).to.equal(filePrice);
+            expect(await fileRegistry.getTokenIdByFileId(fileId)).to.equal(0);
+        });
+
+        it("Should pay for a file and mint token", async function () {
+            const isToken = false;
+            await fileRegistry.recordFile(
+                filePath,
+                fileName,
+                fileSize,
+                filePrice,
+                fileType,
+                fileDescription,
+                isToken
+            );
+
+            const fileId = 0;
+
+            await fileRegistry.connect(buyer).payForFile(fileId, { value: filePrice });
+
+            const [
+                retrievedFilePath,
+                retrievedFileSize,
+                retrievedFilePrice,
+                retrievedFileType,
+                retrievedFileName,
+                retrievedFileDescription,
+                retrievedUploader,
+            ] = await fileRegistry.getFileDataByFileID(fileId);
+
+            expect(buyer.address).to.equal(await token.ownerOf(0));
+
+            const fileIdByTokenId = await fileRegistry.getFileIdByTokenId(0);
+            expect(fileIdByTokenId).to.equal(0);
+        });
     });
 
-    // it("should pay for a file", async function () {
-    //     const filePath = "example/path";
-    //     const fileName = "example_file.txt";
-    //     const fileSize = 1024;
-    //     const filePrice = 100;
-    //     const fileType = "text/plain";
-    //     const fileDescription = "Example file";
-    //     const isToken = true;
-
-    //     await fileRegistry.recordFile(
-    //         filePath,
-    //         fileName,
-    //         fileSize,
-    //         filePrice,
-    //         fileType,
-    //         fileDescription,
-    //         isToken
-    //     );
-
-    //     const fileId = 0;
-
-    //     const payForFileTx = fileRegistry.payForFile(fileId, { value: filePrice });
-    //     await payForFileTx.wait();
-
-    //     const [
-    //         retrievedFilePath,
-    //         retrievedFileSize,
-    //         retrievedFilePrice,
-    //         retrievedFileType,
-    //         retrievedFileName,
-    //         retrievedFileDescription,
-    //         retrievedUploader,
-    //     ] = await fileRegistry.getFileDataByFileID(fileId);
-
-    //     expect(retrievedUploader).to.equal(await ethers.provider.getSigner().getAddress());
-
-    //     const tokensOwnedByMe = await nft.getTokensOwnedByMe();
-    //     expect(tokensOwnedByMe.length).to.equal(1);
-    //     expect(tokensOwnedByMe[0]).to.equal(0);
-
-    //     const tokenCreator = await nft.getTokenCreatorById(0);
-    //     expect(tokenCreator).to.equal(await ethers.provider.getSigner().getAddress());
-    // });
 });
