5d996ad3ec27c6418a532e12ef842fd79c2213b9
raldblox <raldblox@outlook.com>
2023-06-10 04:50:22 +0800

    Added FileRegistry interface for getting fileData

---
+++ hardhat-contracts/contracts/interface/IFileRegistry.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IFileRegistry {
+    function getFileDataByFileID(uint256 _fileID)
+        external
+        view
+        returns (
+            string memory filePath,
+            uint256 fileSize,
+            uint256 filePrice,
+            string memory fileType,
+            string memory fileName,
+            string memory fileDescription,
+            address payable uploader
+        );
+}
