6fd9bb647d56d3f1f3ca44a01c2d5720ab9da810
raldblox <raldblox@outlook.com>
2023-06-12 08:29:02 +0800

    Updated file ban/delist; Added file path history

--- hardhat-contracts/contracts/FileRegistry.sol
+++ hardhat-contracts/contracts/FileRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -27,17 +27,30 @@ contract FileRegistry is IFileRegistry {
         string fileName;
         string fileDescription;
         address payable uploader;
-        bool isBanned;
+        bool isDelistedByOwner;
+        bool isBannedByMod;
     }
 
     mapping(uint256 => File) public files;
     mapping(uint256 => uint256) private _tokenIds;
     mapping(string => address) private originalUploaders;
+    mapping(uint256 => string[]) private filePathHistory; // @note records fileHash history
     mapping(string => uint256[]) private filesByType;
 
-    string[] public fileTypes = ["unknown", "picture", "video", "document", "graphics"];
+    string[] public fileTypes = ["unknown", "picture", "music", "video", "document", "graphics"];
 
     event FileRecorded(
+        uint256 fileId,
+        string filePath,
+        uint256 fileSize,
+        uint256 filePrice,
+        string fileType,
+        string fileName,
+        string fileDescription,
+        address payable uploader
+    );
+
+    event FileUpdated(
         uint256 fileId,
         string filePath,
         uint256 fileSize,
@@ -78,12 +91,9 @@ contract FileRegistry is IFileRegistry {
         require(msg.sender != address(0), "seller must be an address");
         require(_fileSize > 0, "empty file");
 
-    // @dev require that the msg.sender is the original uploader 
-    //if they're going to call this function using an existing filePath
-        require(
-            originalUploaders[_filePath] == address(0) || originalUploaders[_filePath] == msg.sender,
-            "File already exists with a different uploader"
-        );
+        // @dev require that the msg.sender is the original uploader
+        //if they're going to call this function using an existing filePath
+        require(originalUploaders[_filePath] == address(0), "File already exists with a different uploader");
 
         uint256 newFileId = _fileIds.current();
         _fileIds.increment();
@@ -101,9 +111,10 @@ contract FileRegistry is IFileRegistry {
         );
 
         //updates originalUploaders mapping if file is a new entry
-        if (originalUploaders[_filePath] == address(0)) {
-            originalUploaders[_filePath] = msg.sender;
-        }
+        originalUploaders[_filePath] = msg.sender;
+
+        // record filePath for history tracking
+        filePathHistory[newFileId].push(_filePath);
 
         // Add the file ID to the filesByType mapping
         filesByType[_fileType].push(newFileId);
@@ -120,9 +131,63 @@ contract FileRegistry is IFileRegistry {
         );
     }
 
+    function updateFile(
+        uint256 _fileId,
+        string memory _filePath, // @note encrypted hash
+        string memory _fileName,
+        uint256 _fileSize,
+        uint256 _filePrice,
+        string memory _fileType,
+        string memory _fileDescription
+    ) public payable nonReentrant {
+        require(bytes(_filePath).length > 0, "filepath required");
+        require(bytes(_fileType).length > 0, "filetype required");
+        require(bytes(_fileName).length > 0, "filename required");
+        require(validateFileType(_fileType), "Invalid file type");
+        require(_fileSize > 0, "empty file");
+        require(files[_fileId].uploader == msg.sender || isMod[msg.sender], "caller not the file uploader");
+
+        // @dev require that filePath exist
+        require(originalUploaders[_filePath] != address(0), "File path does not exist");
+
+        files[_fileId] = File(
+            newFileId,
+            _filePath,
+            _fileSize,
+            _filePrice,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            payable(msg.sender),
+            false
+        );
+
+        // record originalUploaders of filePath
+        originalUploaders[_filePath] = msg.sender;
+
+        // record filePath for history tracking
+        filePathHistory[_fileId].push(_filePath);
+
+        // Add the file ID to the filesByType mapping
+        filesByType[_fileType].push(_fileId);
+
+        emit FileUpdated(
+            _fileId,
+            _filePath,
+            _fileSize,
+            _filePrice,
+            _fileType,
+            _fileName,
+            _fileDescription,
+            payable(msg.sender)
+        );
+    }
+
     // @dev Allows the user to pay for a file and mint tokens based on the specified token quantity and fileID
     function payForFile(uint256 _fileID, uint256 _tokenQuantity) public payable nonReentrant returns (bool) {
-        require(files[_fileID].isBanned != true, "File is banned");
+        require(files[_fileId].uploader != address(0), "File does not exist");
+        require(files[_fileID].isBannedByMod != true, "File is banned by moderators");
+        require(files[_fileID].isDelistedByOwner != true, "File is delisted by uploader");
 
         // Calculate the total price for the specified token quantity
         uint256 totalPrice = (files[_fileID].filePrice + platformFee) * _tokenQuantity;
@@ -150,14 +215,21 @@ contract FileRegistry is IFileRegistry {
         return true;
     }
 
-    // @dev permanently bans a file
-    function delistFile(uint256 _fileId) public nonReentrant {
-        uint256 fileId = files[_fileId].fileId;
-        require(files[_fileId].fileId > 0, "File has to exist");
-        require(files[_fileId].uploader == msg.sender || mod == msg.sender, "You are not the creator nor the mod");
+    // @dev moderators ban/unban a file
+    function banFile(uint256 _fileId, bool _isBanned) public onlyMod {
+        require(files[_fileId].uploader == address(0), "File has to exist");
 
         //marks the file as Banned
-        files[_fileId].isBanned = true;
+        files[_fileId].isBannedByMod = _isBanned;
+    }
+
+    // @dev file uploader delist a file
+    function delistMyFile(uint256 _fileId, bool _isDelisted) public nonReentrant {
+        require(files[_fileId].uploader == address(0), "File has to exist");
+        require(files[_fileId].uploader == msg.sender, "You are not the creator.");
+
+        //marks the file as Banned
+        files[_fileId].isDelistedByOwner = _isDelisted;
     }
 
     // @dev Returns an array of available files that can be minted.
@@ -173,7 +245,7 @@ contract FileRegistry is IFileRegistry {
         }
 
         File[] memory availableFiles = new File[](count);
-        for (uint256 i = 0; i < count; ++i) {
+        for (uint256 i = 0; i < count; i++) {
             File storage file = files[availableFileIds[i]];
             availableFiles[i] = file;
         }
@@ -182,7 +254,11 @@ contract FileRegistry is IFileRegistry {
     }
 
     function getAvailableFilesByType(string memory _fileType) public view returns (uint256[] memory) {
-    return filesByType[_fileType];
+        return filesByType[_fileType];
+    }
+
+    function getFilePathHistory(string memory _fileId) public view returns (string[] memory) {
+        return filePathHistory[_fileId];
     }
 
     function getFileDataByFileID(
@@ -247,7 +323,7 @@ contract FileRegistry is IFileRegistry {
         emit FileTypeAdded(_fileType);
     }
 
-    // @dev Sets a mod who can cancel a listing that violates rules
+    // @dev Sets mods who can ban/delist file that violates rules
     function setMod(address _mod, bool _isMod) public onlyOwner {
         require(_mod != address(0), "Error: moderator shoudn't be zero address");
         isMod[_mod] = _isMod;
@@ -290,4 +366,4 @@ contract FileRegistry is IFileRegistry {
     function unpause() public onlyOwner {
         unpause();
     }
- }
+}
