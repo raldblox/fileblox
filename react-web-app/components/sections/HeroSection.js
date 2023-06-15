3ab0aa50e938a9ac3c5323a5d9409f9fb975ca7b
raldblox <raldblox@outlook.com>
2023-06-15 13:53:52 +0800

    Updated links

--- react-web-app/components/sections/HeroSection.js
+++ react-web-app/components/sections/HeroSection.js
@@ -54,7 +54,7 @@ export default () => {
                                 <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
                             </svg>
                         </a>
-                        <Link href="/marketplace/create" className="flex items-center justify-center px-4 py-2 font-medium text-gray-700 duration-150 border rounded-lg gap-x-2 hover:text-gray-500 active:bg-gray-100 md:inline-flex">
+                        <Link href="/marketplace/upload" className="flex items-center justify-center px-4 py-2 font-medium text-gray-700 duration-150 border rounded-lg gap-x-2 hover:text-gray-500 active:bg-gray-100 md:inline-flex">
                             Upload Files
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                 <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
