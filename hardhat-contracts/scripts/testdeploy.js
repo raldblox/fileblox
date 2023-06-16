a7e71b9df891cb927b769d2da69f1defbcae287a
raldblox <raldblox@outlook.com>
2023-06-16 13:14:50 +0800

    Updated test deploy script

--- hardhat-contracts/scripts/testdeploy.js
+++ hardhat-contracts/scripts/testdeploy.js
@@ -5,10 +5,11 @@ async function main() {
 
     // Deploy FileRegistry Contract
     const FileRegistry = await ethers.getContractFactory("FileRegistry");
-    const fileRegistry = await FileRegistry.deploy();
+    const fileRegistry = await FileRegistry.deploy("0x328507DC29C95c170B56a1b3A758eB7a9E73455c");
     await fileRegistry.deployed();
     console.log("File Registry:", fileRegistry.address);
     console.log("Token Address:", await fileRegistry.getTokenAddress());
+    console.log("ERC20 Address:", await fileRegistry.getCurrencyAddress());
 }
 
 main()
