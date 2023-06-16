fb3723392149c06258ac27ad1bc54a0152654452
raldblox <raldblox@outlook.com>
2023-06-17 07:24:44 +0800

    Updated dashboard

--- react-web-app/pages/dashboard/index.js
+++ react-web-app/pages/dashboard/index.js
@@ -1,3 +1,4 @@
+import MyTokens from "@/components/sections/MyNFTs"
 import Link from "next/link"
 
 export default () => {
@@ -46,11 +47,8 @@ export default () => {
             <div className="items-start justify-between md:flex">
                 <div className="max-w-lg">
                     <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
-                        All files
+                        My Tokens
                     </h3>
-                    <p className="text-gray-600 mt-2">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    </p>
                 </div>
                 <div className="mt-3 md:mt-0">
                     <Link
@@ -61,42 +59,7 @@ export default () => {
                     </Link>
                 </div>
             </div>
-            <div className="mt-12 relative h-max overflow-auto">
-                <table className="w-full table-auto text-sm text-left">
-                    <thead className="text-gray-600 font-medium border-b">
-                        <tr>
-                            <th className="py-3 pr-6">File Name</th>
-                            <th className="py-3 pr-6">Size</th>
-                            <th className="py-3 pr-6">Status</th>
-                            <th className="py-3 pr-6">Earnings</th>
-                            <th className="py-3 pr-6">Price</th>
-
-                        </tr>
-                    </thead>
-                    <tbody className="text-gray-600 divide-y">
-                        {
-                            tableItems.map((item, idx) => (
-                                <tr key={idx}>
-                                    <td className="pr-6 py-4 whitespace-nowrap">{item.name}</td>
-                                    <td className="pr-6 py-4 whitespace-nowrap">{item.size}</td>
-                                    <td className="pr-6 py-4 whitespace-nowrap">
-                                        <span className={`px-3 py-2 rounded-full font-semibold text-xs ${item.status == "Active" ? "text-green-600 bg-green-50" : "text-blue-600 bg-blue-50"}`}>
-                                            {item.status}
-                                        </span>
-                                    </td>
-                                    <td className="pr-6 py-4 whitespace-nowrap">{item.tokenID}</td>
-                                    <td className="pr-6 py-4 whitespace-nowrap">{item.price}</td>
-                                    <td className="text-right whitespace-nowrap">
-                                        <a href="javascript:void()" className="py-1.5 px-3 text-gray-600 hover:text-gray-500 duration-150 hover:bg-gray-50 border rounded-lg">
-                                            Manage
-                                        </a>
-                                    </td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
-            </div>
+            <MyTokens />
         </div>
     )
 }
